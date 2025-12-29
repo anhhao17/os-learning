6f1458a47ba8242f48502e0f35e63fdf670a3be3
anhhao17 <hiimanhhao@gmail.com>
2025-12-29 22:51:46 +0700

    ld, boot.s: The Global Constructor Bridge

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,17 +1,12 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
+/* --- Safety Checks --- */
 #if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#error "You are not using a cross-compiler! Use i686-elf-gcc instead."
 #endif
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
-
-/* Hardware text mode color constants. */
+/* --- VGA Constants & Types --- */
 enum vga_color {
     VGA_COLOR_BLACK = 0,
     VGA_COLOR_BLUE = 1,
@@ -31,83 +26,82 @@ enum vga_color {
     VGA_COLOR_WHITE = 15,
 };
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
-{
-    return fg | bg << 4;
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+    return fg | (bg << 4);
 }
 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
-{
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
     return (uint16_t) uc | (uint16_t) color << 8;
 }
 
-size_t strlen(const char* str)
-{
-    size_t len = 0;
-    while (str[len])
-        len++;
-    return len;
-}
+/* --- The Terminal Class --- */
+class Terminal {
+private:
+    size_t row;
+    size_t col;
+    uint8_t color;
+    uint16_t* buffer;
+    static const size_t WIDTH = 80;
+    static const size_t HEIGHT = 25;
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+public:
+    Terminal() {
+        row = 0;
+        col = 0;
+        color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+        buffer = (uint16_t*) 0xB8000;
+        clear();
+    }
 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
-
-void terminal_initialize(void)
-{
-    terminal_row = 0;
-    terminal_column = 0;
-    terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    terminal_buffer = (uint16_t*) 0xB8000;
-    for (size_t y = 0; y < VGA_HEIGHT; y++) {
-        for (size_t x = 0; x < VGA_WIDTH; x++) {
-            const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = vga_entry(' ', terminal_color);
+    void clear() {
+        for (size_t y = 0; y < HEIGHT; y++) {
+            for (size_t x = 0; x < WIDTH; x++) {
+                const size_t index = y * WIDTH + x;
+                buffer[index] = vga_entry(' ', color);
+            }
         }
     }
-}
 
-void terminal_setcolor(uint8_t color)
-{
-    terminal_color = color;
-}
-
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
-{
-    const size_t index = y * VGA_WIDTH + x;
-    terminal_buffer[index] = vga_entry(c, color);
-}
-
-void terminal_putchar(char c)
-{
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    if (++terminal_column == VGA_WIDTH) {
-        terminal_column = 0;
-        if (++terminal_row == VGA_HEIGHT)
-            terminal_row = 0;
+    void set_color(uint8_t new_color) {
+        color = new_color;
     }
-}
 
-void terminal_write(const char* data, size_t size)
-{
-    for (size_t i = 0; i < size; i++)
-        terminal_putchar(data[i]);
-}
+    void put_char(char c) {
+        if (c == '\n') {
+            col = 0;
+            if (++row == HEIGHT) row = 0;
+            return;
+        }
 
-void terminal_writestring(const char* data)
-{
-    terminal_write(data, strlen(data));
-}
+        const size_t index = row * WIDTH + col;
+        buffer[index] = vga_entry(c, color);
 
-extern "C" void kernel_main(void)
-{
-    /* Initialize terminal interface */
-    terminal_initialize();
+        if (++col == WIDTH) {
+            col = 0;
+            if (++row == HEIGHT) row = 0;
+        }
+    }
 
-    /* Newline support is left as an exercise. */
-    terminal_writestring("Hello, kernel World!");
+    void write(const char* data) {
+        for (size_t i = 0; data[i] != '\0'; i++) {
+            put_char(data[i]);
+        }
+    }
+};
+
+/* --- Global Instances --- */
+// Because we updated boot.s and linker.ld, this constructor WILL run.
+Terminal tty;
+
+/* --- Kernel Entry Point --- */
+extern "C" void kernel_main(void) {
+    // We don't need to call terminal_initialize()! 
+    // The global constructor already did it.
+    
+    tty.write("C++ Global Constructor Test: SUCCESS\n");
+    tty.set_color(vga_entry_color(VGA_COLOR_LIGHT_CYAN, VGA_COLOR_BLACK));
+    tty.write("Hello from the Terminal Class!\n");
+    
+    tty.set_color(vga_entry_color(VGA_COLOR_LIGHT_RED, VGA_COLOR_BLACK));
+    tty.write("Newline support is active.\n");
 }
